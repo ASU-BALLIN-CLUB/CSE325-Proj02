32b4b544e406699e4bf22d0592d2bedb1ff52ec7
Slamkas <seslamka@yahoo.com>
2014-09-17 12:47:03 -0700

    Create uc_led.c

---
+++ uc_led.c
@@ -0,0 +1,33 @@
+//
+//  uc_led.c
+//
+//
+//  Created by Sean Slamka on 9/17/14.
+//
+//  Lab Partners: Sean Slamka, Aydin Balci
+
+#include "uc_led.h"
+
+void uc_led_all_off()
+{
+}
+
+void uc_led_all_on()
+{
+}
+
+void uc_led_init()
+{
+}
+
+void uc_led_off(int p_led)
+{
+}
+
+void uc_led_on(int p_led)
+{
+}
+
+void uc_led_toggle(int p_led)
+{
+}
