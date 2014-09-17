7f1e709f3c01c4d808b21c2bf0cdad96498c53d7
Slamkas <seslamka@yahoo.com>
2014-09-17 12:46:01 -0700

    Create main.c

---
+++ main.c
@@ -0,0 +1,29 @@
+//
+//  main.c
+//	Proj02
+//
+//  Created by Sean Slamka on 9/17/14.
+//
+//  Lab Partners: Sean Slamka, Aydin Balci
+
+static void count_up(int p_delay)
+{
+}
+static int dipsw_delay_poll()
+{
+}
+static void dipsw_onoff_poll()
+{
+}
+static void hw_init()
+{
+}
+static void leds_on(int const p_value)
+{
+}
+__declspec(noreturn) int main()
+{
+}
+static void run()
+{
+}
