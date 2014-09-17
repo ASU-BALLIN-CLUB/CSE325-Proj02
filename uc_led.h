b79c73d79bff19da682752f35e9d0263045d46fd
Slamkas <seslamka@yahoo.com>
2014-09-17 12:47:25 -0700

    Create uc_led.h

---
+++ uc_led.h
@@ -0,0 +1,20 @@
+//
+//  uc_led.h
+//
+//
+//  Created by Sean Slamka on 9/17/14.
+//
+//  Lab Partners: Sean Slamka, Aydin Balci
+
+#ifndef ____uc_led__
+#define ____uc_led__
+
+//#include <iostream>
+//#include "Token.h"
+
+typedef enum {
+ uc_led_1 = 1, // Represents LED1
+ uc_led_2 = 2, // Represents LED2
+ uc_led_3 = 3, // Represents LED3
+ uc_led_4 = 4 // Represents LED4
+} uc_led_t;
