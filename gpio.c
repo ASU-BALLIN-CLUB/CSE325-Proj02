39d52f41ea963bcceb5ea263c9796d4cf478e973
Slamkas <seslamka@yahoo.com>
2014-09-17 14:13:29 -0700

    Update gpio.c

--- gpio.c
+++ gpio.c
@@ -26,13 +26,51 @@ void gpio_port_init(gpio_port_t p_port, gpio_pin_t p_pin, gpio_funct_t p_funct, 
 	 switch (p_port)
 	 {
 	 	case gpio_port_dd:
-	 		GPIO_PAR(p-port) |= 0x0;
-	 		GPIO_PAR(p_port)
+	 		GPIO_PAR(p-port) &= 0x0;
+	 		GPIO_PAR(p_port) |= 0x0;
 	 		break;
 	 	case gpio_port_tc:
-	 		GPIO_PAR)p_port) |= 0x0;
-	 		APIO_PAR(p_port)
+	 		GPIO_PAR(p_port) &= 0x0;
+	 		GPIO_PAR(p_port) |= 0x0;
 	 		break;
 	 	default:
-
+	 		break;
 	}
+	if(p_funct == 0)
+	{
+		gpio_port_set_data_dir(p_port, p_pin, p_data_dir)
+		if(p_data_dir == 1)
+		{
+			gpio_port_set_pin_state(p_port,p_pin,p_state)
+		}
+	}
+}
+gpio_pin_state_t gpio_port_get_pin_state(gpio_port_t p_port, gpio_pin_t p_pin)
+{
+	gpio_pin_state_t state = GPIO_SET(p_port) >> p_pin;
+	return state;
+}
+static void gpio_port_set_data_dir(gpio_port_t p_port, gpio_pin_t p_pin, gpio_data_dir_t p_data_dir)
+{
+	if(p_data_dir == gpio_data_dir_in)
+	{
+		GPIO_DDR(p_port) &= 0;
+	}
+	else if (p_data_dir == gpio_data_dir_out)
+	{
+		GPIO_DDR(p_port) |= 1;
+	}
+}
+void gpio_port_set_pin_state(gpio_port_t p_port, gpio_pin_t p_pin, gpio_pin_state_t p_state)
+{
+	if(p_state == gpio_pin_state_high)
+	{
+		GPIO_SET(p_port) |= p_pin;
+	}
+	else
+	{
+		GPIO_CLR(p_port) &= 0;
+	}
+}
+
+
