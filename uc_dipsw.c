10ae57904afcadb621dcefca2226f24781323ac8
Slamkas <seslamka@yahoo.com>
2014-09-17 12:46:18 -0700

    Create uc_dipsw.c

---
+++ uc_dipsw.c
@@ -0,0 +1,17 @@
+//
+//  uc_dipsw.c
+//
+//
+//  Created by Sean Slamka on 9/17/14.
+//
+//  Lab Partners: Sean Slamka, Aydin Balci
+
+#include "uc_dipsw.h"
+
+uc_dipsw_state_t uc_dipsw_get_state(uc_dipsw_t p_switch)
+{
+}
+
+void uc_dipsw_init()
+{
+}
