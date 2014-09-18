8bb0edeb5c2c7fa4564e1a05371e1eda8d666eda
Slamkas <seslamka@yahoo.com>
2014-09-17 17:52:18 -0700

    Update uc_dipsw.c

--- uc_dipsw.c
+++ uc_dipsw.c
@@ -10,8 +10,15 @@
 
 uc_dipsw_state_t uc_dipsw_get_state(uc_dipsw_t p_switch)
 {
+	 uc_dipsw_state_t state = gpio_port_get_pin_state(gpio_port_dd, p_switch);
+	 return state;
 }
 
 void uc_dipsw_init()
 {
+
+	gpio_port_init(gpio_port_dd , gpio_pin_4, gpio_funct_gpio, gpio_data_dir_in, uc_dipsw_state_on);
+	gpio_port_init(gpio_port_dd , gpio_pin_5, gpio_funct_gpio, gpio_data_dir_in, uc_dipsw_state_on);
+	gpio_port_init(gpio_port_dd , gpio_pin_6, gpio_funct_gpio, gpio_data_dir_in, uc_dipsw_state_on);
+	gpio_port_init(gpio_port_dd , gpio_pin_7, gpio_funct_gpio, gpio_data_dir_in, uc_dipsw_state_on);
 }
