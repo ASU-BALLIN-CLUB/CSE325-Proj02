4dc8998f33b4d53715fa6808a6169ed734da4a6b
abalci <abalci@asu.edu>
2014-10-08 10:59:03 -0700

    Update uc_led.h

--- uc_led.h
+++ uc_led.h
@@ -1,16 +1,17 @@
 //
 //  uc_led.h
 //
-//
-//  Created by Sean Slamka on 9/17/14.
-//
+//	Proj02
 //  Lab Partners: Sean Slamka, Aydin Balci
-
+//  Email: sslamka@asu.edu, abalci@asu.edu
+//  CSE325 Embedded Microprocessor Systems
+//  Fall 2014
+//
 #ifndef ____uc_led__
 #define ____uc_led__
 
 #include "gpio.h"
-
+#include "support_common.h"
 
 typedef enum {
  uc_led_1 = 1, // Represents LED1
@@ -18,6 +19,12 @@ typedef enum {
  uc_led_3 = 3, // Represents LED3
  uc_led_4 = 4 // Represents LED4
 } uc_led_t;
-};
 
-#endif /* defined(__uc_led__) */
+void uc_led_all_off();
+void uc_led_all_on();
+void uc_led_init();
+void uc_led_off(gpio_pin_t p_led);
+void uc_led_on(gpio_pin_t p_led);
+void uc_led_toggle(gpio_pin_t p_led);
+
+#endif /* defined (__uc_led__) */
